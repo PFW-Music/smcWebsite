587aa09762750d0297ec6ae3f4c43b43b3f51d59
priyankvlogs <priyankvlogs@gmail.com>
2023-04-08 11:44:15 -0400

    Remove Old Files

--- src/components/airtable.js
+++
@@ -1,7 +0,0 @@
-import Airtable from 'airtable';
-
-const base = new Airtable({
-  apiKey: process.env.NEXT_PUBLIC_AIRTABLE_KEY,
-}).base(process.env.NEXT_PUBLIC_AIRTABLE_BASE_ID);
-
-export default base;
