118995c904723317ddede12cb89c87a06527105a
priyankvlogs <priyankvlogs@gmail.com>
2023-04-11 19:49:04 -0400

    Fixed GearFilter Maybe?

--- src/components/TimeInput/TimeInput.js
+++ src/components/TimeInput/TimeInput.js
@@ -8,20 +8,11 @@ import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
 const Alert = React.forwardRef(function Alert(props, ref) {
-	return (
-		<MuiAlert
-			elevation={6}
-			ref={ref}
-			variant="filled"
-			horizontal="center"
-			{...props}
-		/>
-	);
+	return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
 });
 
 const roundToNearestHalfHour = (date) => {
-	const minutes = date.getMinutes();
-	const roundedMinutes = minutes >= 30 ? 60 : 30;
+	const roundedMinutes = date.getMinutes() >= 30 ? 60 : 30;
 	date.setMinutes(roundedMinutes);
 	date.setSeconds(0);
 	date.setMilliseconds(0);
@@ -32,211 +23,52 @@ const addHours = (date, hours) => {
 	return new Date(date.getTime() + hours * 60 * 60 * 1000);
 };
 
-function useTimeSelection() {
+const useTimeSelection = () => {
 	const [startDate, setStartDate] = useState(new Date());
 	const [endDate, setEndDate] = useState(new Date());
 
 	useEffect(() => {
 		const roundedDate = roundToNearestHalfHour(new Date());
 		setStartDate(roundedDate);
-
-		const oneHourForward = addHours(roundedDate, 1);
-		setEndDate(oneHourForward);
+		setEndDate(addHours(roundedDate, 1));
 	}, []);
 
 	const handleDateChange = (type, date) => {
 		if (type === "start") {
 			setStartDate(date);
-
-			const oneHourForward = addHours(date, 1);
-			setEndDate(oneHourForward);
+			setEndDate(addHours(date, 1));
 		} else {
 			setEndDate(date);
 		}
 	};
 
 	return { startDate, endDate, handleDateChange };
-}
+};
 
-function filterTemporallyUnavailableGear(
+const filterTemporallyUnavailableGear = (
 	gearList,
 	startTimeSelected,
 	endTimeSelected
-) {
-	let temporallyFilteredGear = [];
-	let eventStart = null;
-	let eventEnd = null;
-	let conflictFound = false;
+) => {
+	const startTime = new Date(startTimeSelected);
+	const endTime = new Date(endTimeSelected);
 
-	for (let gearIndex = 0; gearIndex < gearList.length; gearIndex++) {
-		conflictFound = false;
+	return gearList.filter((gear) => {
+		if (!gear.eventStart || !gear.eventEnd) return true;
 
-		//No events
-		if (
-			typeof gearList[gearIndex].eventStart === "undefined" ||
-			typeof gearList[gearIndex].eventEnd === "undefined"
-		) {
-			temporallyFilteredGear.push(gearList[gearIndex]);
-			continue;
-		}
+		return !gear.eventStart.some((eventStart, idx) => {
+			const eventEnd = gear.eventEnd[idx];
+			const eventStartDate = new Date(eventStart);
+			const eventEndDate = new Date(eventEnd);
 
-		//loop through start and end times
-		for (
-			let eventIndex = 0;
-			eventIndex < gearList[gearIndex].eventStart.length && !conflictFound;
-			eventIndex++
-		) {
-			eventStart = gearList[gearIndex].eventStart[eventIndex];
-			eventEnd = gearList[gearIndex].eventEnd[eventIndex];
-
-			//Chosen start or end is in the middle of an existing event
-			conflictFound =
-				conflictFound ||
-				(startTimeSelected > eventStart && startTimeSelected < eventEnd);
-			conflictFound =
-				conflictFound ||
-				(endTimeSelected > eventStart && endTimeSelected < eventEnd);
-			//Chosen start is before event and chosen end is after event
-			conflictFound =
-				conflictFound ||
-				(startTimeSelected < eventStart && endTimeSelected > eventEnd);
-		}
-		if (!conflictFound) {
-			temporallyFilteredGear.push(gearList[gearIndex]);
-		}
-	}
-	return temporallyFilteredGear;
-}
-
-function DateTimeValidation({
-	setTimeCorrect,
-	setStartTimeSelected,
-	setEndTimeSelected,
-	roomBookingRecord,
-	gearList,
-	setFilteredGearList,
-}) {
-	const [roomUnavailable, setRoomUnavailable] = useState(false);
-	const [successMsg, setSuccessMsg] = useState(false);
-	const [unavailableRoom, setUnavailableRoom] = useState("");
-
-	const { startDate, endDate, handleDateChange } = useTimeSelection();
-
-	const handleClose = (event, reason) => {
-		if (reason === "clickaway") {
-			return;
-		}
-		setSuccessMsg(false);
-	};
-
-	const handleAvailabilityCheck = () => {
-		const startTime = startDate.toISOString();
-		const endTime = endDate.toISOString();
-
-		if (startTime > endTime) {
-			setTimeCorrect(false);
-			return;
-		}
-		setTimeCorrect(true);
-		setStartTimeSelected(startTime);
-		setEndTimeSelected(endTime);
-
-		const conflictFound = checkRoomAvailability(
-			startTime,
-			endTime,
-			roomBookingRecord
-		);
-
-		if (conflictFound) {
-			setRoomUnavailable(true);
-			setSuccessMsg(false);
-			setTimeCorrect(false);
-		} else {
-			setRoomUnavailable(false);
-			setSuccessMsg(true);
-			setTimeCorrect(true);
-
-			// Call filterTemporallyUnavailableGear here with the updated start and end times
-			setFilteredGearList(
-				filterTemporallyUnavailableGear(gearList, startTime, endTime)
+			return (
+				(startTime >= eventStartDate && startTime < eventEndDate) ||
+				(endTime > eventStartDate && endTime <= eventEndDate) ||
+				(startTime <= eventStartDate && endTime >= eventEndDate)
 			);
-		}
-	};
-
-	const checkRoomAvailability = (startTime, endTime, roomBookingRecord) => {
-		let realEndTime = new Date(endTime);
-		realEndTime.setHours(realEndTime.getHours());
-		realEndTime = realEndTime.toISOString();
-
-		let conflictFound = false;
-
-		if (typeof roomBookingRecord === "undefined") {
-			return conflictFound;
-		}
-
-		for (const record of roomBookingRecord) {
-			if (typeof record.eventStart === "undefined") continue;
-
-			for (let j = 0; j < record.eventStart.length; j++) {
-				if (record.eventStatus[j] !== "Booked ✅") continue;
-
-				if (
-					startTime <= record.eventStart[j] &&
-					realEndTime >= record.eventEnd[j]
-				) {
-					conflictFound = true;
-					setUnavailableRoom(record.name);
-					break;
-				} else if (
-					startTime >= record.eventStart[j] &&
-					startTime <= record.eventEnd[j]
-				) {
-					conflictFound = true;
-					setUnavailableRoom(record.name);
-					break;
-				} else if (
-					realEndTime > record.eventStart[j] &&
-					realEndTime < record.eventEnd[j]
-				) {
-					conflictFound = true;
-					setUnavailableRoom(record.name);
-					break;
-				}
-			}
-
-			if (conflictFound) {
-				break;
-			}
-		}
-
-		return conflictFound;
-	};
-
-	return (
-		<div>
-			<Stack spacing={1}>
-				<DatePickerSection
-					startDate={startDate}
-					endDate={endDate}
-					handleDateChange={handleDateChange}
-				/>
-			</Stack>
-
-			<Box justifyContent="center" alignItems="center">
-				<br />
-				<Button color="warning" auto ghost onClick={handleAvailabilityCheck}>
-					check availability
-				</Button>
-			</Box>
-			<NotificationSection
-				roomUnavailable={roomUnavailable}
-				successMsg={successMsg}
-				handleClose={handleClose}
-				unavailableRoom={unavailableRoom}
-			/>
-		</div>
-	);
-}
+		});
+	});
+};
 
 const DatePickerSection = ({ startDate, endDate, handleDateChange }) => {
 	const filterEndTime = (time) => {
@@ -316,6 +148,112 @@ const NotificationSection = ({
 					<Alert severity="success">Room is available at inputted time</Alert>
 				</Snackbar>
 			)}
+		</div>
+	);
+};
+
+const DateTimeValidation = ({
+	setTimeCorrect,
+	setStartTimeSelected,
+	setEndTimeSelected,
+	roomBookingRecord,
+	gearList,
+	setFilteredGearList,
+}) => {
+	const [roomUnavailable, setRoomUnavailable] = useState(false);
+	const [successMsg, setSuccessMsg] = useState(false);
+	const [unavailableRoom, setUnavailableRoom] = useState("");
+
+	const { startDate, endDate, handleDateChange } = useTimeSelection();
+
+	const handleClose = (event, reason) => {
+		if (reason === "clickaway") {
+			return;
+		}
+		setSuccessMsg(false);
+	};
+
+	const handleAvailabilityCheck = () => {
+		const startTime = startDate.toISOString();
+		const endTime = endDate.toISOString();
+
+		if (startTime > endTime) {
+			setTimeCorrect(false);
+			return;
+		}
+		setTimeCorrect(true);
+		setStartTimeSelected(startTime);
+		setEndTimeSelected(endTime);
+
+		const conflictFound = checkRoomAvailability(
+			startTime,
+			endTime,
+			roomBookingRecord
+		);
+
+		if (conflictFound) {
+			setRoomUnavailable(true);
+			setSuccessMsg(false);
+			setTimeCorrect(false);
+		} else {
+			setRoomUnavailable(false);
+			setSuccessMsg(true);
+			setTimeCorrect(true);
+
+			setFilteredGearList(
+				filterTemporallyUnavailableGear(gearList, startTime, endTime)
+			);
+		}
+	};
+
+	const checkRoomAvailability = (startTime, endTime, roomBookingRecord) => {
+		const realEndTime = new Date(endTime).toISOString();
+
+		if (!roomBookingRecord) return false;
+
+		return roomBookingRecord.some((record) => {
+			if (!record.eventStart) return false;
+
+			return record.eventStart.some((eventStart, idx) => {
+				if (record.eventStatus[idx] !== "Booked ✅") return false;
+
+				const eventEnd = record.eventEnd[idx];
+
+				if (
+					(startTime <= eventStart && realEndTime >= eventEnd) ||
+					(startTime >= eventStart && startTime <= eventEnd) ||
+					(realEndTime > eventStart && realEndTime < eventEnd)
+				) {
+					setUnavailableRoom(record.name);
+					return true;
+				}
+				return false;
+			});
+		});
+	};
+
+	return (
+		<div>
+			<Stack spacing={1}>
+				<DatePickerSection
+					startDate={startDate}
+					endDate={endDate}
+					handleDateChange={handleDateChange}
+				/>
+			</Stack>
+
+			<Box justifyContent="center" alignItems="center">
+				<br />
+				<Button color="warning" auto ghost onClick={handleAvailabilityCheck}>
+					check availability
+				</Button>
+			</Box>
+			<NotificationSection
+				roomUnavailable={roomUnavailable}
+				successMsg={successMsg}
+				handleClose={handleClose}
+				unavailableRoom={unavailableRoom}
+			/>
 		</div>
 	);
 };
