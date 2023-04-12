5d547cb1155fd950cade8708a64eb36898d2fe60
priyankvlogs <priyankvlogs@gmail.com>
2023-04-11 22:59:12 -0400

    Updated Gear Page

--- src/components/SubmitButton/IndividualSubmit.js
+++ src/components/SubmitButton/IndividualSubmit.js
@@ -1,28 +1,81 @@
 import React from "react";
 import base from "../airtable";
+import { Button as NextButton } from "@nextui-org/react";
+import Modal from "@mui/material/Modal";
+import Backdrop from "@mui/material/Backdrop";
+import Fade from "@mui/material//Fade";
+import Typography from "@mui/material/Typography";
 
-async function createEventRecord(
+async function createRecord(
 	users,
+	sessionTitle,
+	eventTypeSelected,
+	faculties,
+	usageSelected,
 	startTimeSelected,
 	endTimeSelected,
+	courses,
 	gears
 ) {
 	try {
 		const records = await base("Events").create([
 			{
 				fields: {
-					"Event Name": "Individual Gear Check Out",
+					"Event Name": sessionTitle,
 					"Start Time": startTimeSelected,
 					"Proposed End Time": endTimeSelected,
+					Class: courses,
+					"Event Type": eventTypeSelected,
+					Faculty: faculties,
 					Students: users,
 					Status: "Booked ✅",
+					"Intent of Use": usageSelected,
 					"Gear Selection": gears,
 				},
 			},
 		]);
 
 		records.forEach(function (record) {
-			return record.getId();
+			console.log(record.getId());
+		});
+	} catch (err) {
+		console.error(err);
+	}
+}
+
+async function updateRecord(
+	eventID,
+	users,
+	sessionTitle,
+	eventTypeSelected,
+	faculties,
+	usageSelected,
+	startTimeSelected,
+	endTimeSelected,
+	courses,
+	gears
+) {
+	try {
+		const records = await base("Events").update([
+			{
+				id: eventID,
+				fields: {
+					"Event Name": sessionTitle,
+					"Start Time": startTimeSelected,
+					"Proposed End Time": endTimeSelected,
+					Class: courses,
+					"Event Type": eventTypeSelected,
+					Students: users,
+					Faculty: faculties,
+					Status: "Booked ✅",
+					"Intent of Use": usageSelected,
+					"Gear Selection": gears,
+				},
+			},
+		]);
+
+		records.forEach(function () {
+			console.log("record updated");
 		});
 	} catch (err) {
 		console.error(err);
@@ -31,42 +84,111 @@ async function createEventRecord(
 
 export default function Submit({
 	userSelected,
+	sessionTitle,
+	eventTypeSelected,
+	facultySelected,
+	usageSelected,
 	startTimeSelected,
 	endTimeSelected,
+	courseSelected,
 	gearSelected,
+
+	eventID,
+	newEvent,
+	updateEvent,
+	setCancelEvent,
 	timeCorrect,
+	setUserCount,
+	setAddCourse,
+	setAddGear,
 }) {
+	const [open, setOpen] = React.useState(false);
+
 	const handleSubmit = async () => {
+		setOpen(true);
+
 		const users = userSelected.map((obj) => obj.id);
+		const faculties = facultySelected.map((obj) => obj.id);
+		const courses = courseSelected.map((obj) => obj.key);
 		const gears = gearSelected.map((obj) => obj.id);
 
-		await createEventRecord(users, startTimeSelected, endTimeSelected, gears);
-		// Add any desired action after submission, e.g., show an alert or reload the page
-		alert(
-			"Submission Successful! Please check your inbox for booking confirmation."
-		);
+		if (newEvent) {
+			await createRecord(
+				users,
+				sessionTitle,
+				eventTypeSelected,
+				faculties,
+				usageSelected,
+				startTimeSelected,
+				endTimeSelected,
+				courses,
+				gears
+			);
+		} else if (updateEvent) {
+			await updateRecord(
+				eventID,
+				users,
+				sessionTitle,
+				eventTypeSelected,
+				faculties,
+				usageSelected,
+				startTimeSelected,
+				endTimeSelected,
+				courses,
+				gears
+			);
+		}
+	};
+
+	const handleClose = () => {
+		setUserCount(0);
+		setAddCourse(false);
+		setAddGear(false);
+
+		setOpen(false);
 		window.location.reload();
 	};
 
 	return (
 		<div className="flex flex-col items-center justify-center gap-4">
-			<button
-				className={`${
-					!(endTimeSelected && startTimeSelected && timeCorrect) ||
-					gearSelected.length === 0 ||
-					userSelected.length === 0
-						? "bg-gray-300 cursor-not-allowed"
-						: "bg-yellow-500 hover:bg-yellow-600"
-				} text-white font-semibold py-2 px-4 border border-yellow-500 rounded`}
+			<NextButton
+				bordered
+				color="warning"
 				onClick={handleSubmit}
 				disabled={
-					!(endTimeSelected && startTimeSelected && timeCorrect) ||
-					gearSelected.length === 0 ||
-					userSelected.length === 0
+					!(
+						sessionTitle &&
+						eventTypeSelected &&
+						endTimeSelected &&
+						startTimeSelected &&
+						timeCorrect
+					)
 				}
 			>
 				SUBMIT
-			</button>
+			</NextButton>
+
+			<Modal
+				className="flex items-center justify-center"
+				open={open}
+				onClose={handleClose}
+				closeAfterTransition
+				BackdropComponent={Backdrop}
+				BackdropProps={{
+					timeout: 500,
+				}}
+			>
+				<Fade in={open}>
+					<div className="bg-neutral-900 inline-block p-6 my-8 border border-yellow-500 text-white overflow-hidden shadow-xl transform transition-all">
+						<Typography variant="h5" className="font-bold mb-2">
+							Submission Successful!
+						</Typography>
+						<Typography>
+							Please check your inbox for booking confirmation.
+						</Typography>
+					</div>
+				</Fade>
+			</Modal>
 		</div>
 	);
 }
--- src/pages/gear.js
+++ src/pages/gear.js
@@ -1,137 +1,268 @@
-"use client";
 import React from "react";
-import IndividualNameInput from "../components/NameInput/IndividualNameInput";
-import IndividualTimeInput from "../components/TimeInput/IndividualTimeInput";
-import IndividualGearCheckOut from "../components/GearInput/IndividualGearCheckOut";
-import IndividualSubmit from "../components/SubmitButton/IndividualSubmit";
-
-import Paper from "@mui/material/Paper";
-import Box from "@mui/material/Box";
-import Collapse from "@mui/material/Collapse";
-import FormLabel from "@mui/material/FormLabel";
 import base from "../components/airtable";
-
-///////////////////////////////////////////             ///////////////////////////////////////////
-///////////////////////////////////////////  API CALLS  ///////////////////////////////////////////
-///////////////////////////////////////////             ///////////////////////////////////////////
+import NameInput from "../components/NameInput";
+import EventDetailsInput from "../components/EventDetailsInput";
+import TimeInput from "../components/TimeInput";
+import GearCheckOut from "../components/GearCheckOut";
+import FormActions from "../components/FormActions";
+import EventID from "../components/EventID";
+import Submit from "../components/SubmitButton/IndividualSubmit";
+import IframeSlide from "../components/IframeSlide";
+import HeaderWithSubtitle from "../components/HeaderWithSubtitle";
+import Collapse from "@mui/material/Collapse";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import FormLabel from "@mui/material/FormLabel";
+import { Container, Card, Row, Text } from "@nextui-org/react";
 
 const peopleAllInfo = [];
+const SMCpeople = [];
+const facultyList = [];
 
 let x = 0;
-///////////////////////Pulling records from SMC People///////////////////////
 base("SMC People")
 	.select({
 		view: "ALL PEOPLE",
 	})
 	.eachPage(
 		function page(records, fetchNextPage) {
-			// This function (`page`) will get called for each page of records.
-
 			records.forEach(function (record) {
+				SMCpeople.push({ name: record.get("Person"), id: record.id });
 				peopleAllInfo[x] = {
 					id: record.id,
 					name: record.get("Person"),
+					roomAccess: record.get("Room Access"),
 					gearAccess: record.get("Gear Access"),
+					phoneNum: record.get("Phone"),
 				};
+
 				x = x + 1;
+
+				if (record.get("Role").includes("Faculty/Staff 🎓")) {
+					facultyList.push({ name: record.get("Person"), id: record.id });
+				}
 			});
 
-			// To fetch the next page of records, call `fetchNextPage`.
-			// If there are more records, `page` will get called again.
-			// If there are no more records, `done` will get called.
 			fetchNextPage();
 		},
 		function done(err) {
 			if (err) {
+				console.error(err);
 			}
 		}
 	);
 
-function Gear1() {
-	const [userSelected, setUserSelected] = React.useState([]);
-	const [gear, setGear] = React.useState([]);
-	const [gearList, setGearList] = React.useState([]);
-	const [gearSelected, setGearSelected] = React.useState([]);
-	const [startTimeSelected, setStartTimeSelected] = React.useState("");
-	const [endTimeSelected, setEndTimeSelected] = React.useState("");
-
-	const [timeCorrect, setTimeCorrect] = React.useState(false);
-
-	const nameInput = (
-		<Paper sx={{ maxWidth: 700, width: "90%", my: 2, mx: "auto", p: 2 }}>
-			<Box sx={{ textAlign: "center", m: 2, fontSize: 22, lineHeight: 2 }}>
-				Who is booking?
-				<br />
-				<FormLabel component="legend">
-					Add users to see a list of gear available to your access level in the
-					Select Gear menu.
+const InputSection = ({ title, description, children }) => (
+	<Paper className="my-2 mx-auto p-2">
+		<Box className="text-center m-2 text-xl">{title}</Box>
+		{description && (
+			<Box className="text-sm">
+				<FormLabel component="legend" className="ml-4">
+					{description}
 				</FormLabel>
 			</Box>
-			<IndividualNameInput
+		)}
+		{children}
+	</Paper>
+);
+
+const SMCHours = () => (
+	<Container className="bg-neutral-900 text-white flex items-center justify-center">
+		<Card.Body>
+			<Text className="text-center text-2xl">SMC Hours & Availability</Text>
+			<Row className="text-xl justify-center">
+				<div className="columns-1">
+					<Text>Monday — Friday: </Text>
+					<Text>8:00 AM — Midnight</Text>
+				</div>
+				<div className="columns-2" />
+				<div className="Columns-3">
+					<Text>Saturday & Sunday: </Text>
+					<Text>12:00 PM — Midnight</Text>
+				</div>
+			</Row>
+		</Card.Body>
+	</Container>
+);
+
+export default function Home() {
+	const [userSelected, setUserSelected] = React.useState([]);
+	const [sessionTitle, setSessionTitle] = React.useState("");
+	const [eventTypeSelected, setEventTypeSelected] = React.useState([]);
+	const [facultySelected, setFacultySelected] = React.useState([]);
+	const [usageSelected, setUsageSelected] = React.useState([]);
+	const [startTimeSelected, setStartTimeSelected] = React.useState("");
+	const [endTimeSelected, setEndTimeSelected] = React.useState("");
+	const [courseSelected, setCourseSelected] = React.useState([]);
+	const [gearSelected, setGearSelected] = React.useState([]);
+
+	const [IDerror, setIDError] = React.useState(false);
+	const [eventID, setEventID] = React.useState("");
+	const [goodID, setGoodID] = React.useState(false);
+
+	const [userCount, setUserCount] = React.useState(0);
+	const [disabledRoomTypes, setDisabledRoomTypes] = React.useState([]);
+	const [timeCorrect, setTimeCorrect] = React.useState(false);
+	const [gearList, setGearList] = React.useState([]);
+	const [filteredGearList, setFilteredGearList] = React.useState([]);
+
+	const [addGear, setAddGear] = React.useState(false);
+	const [addCourse, setAddCourse] = React.useState(false);
+
+	const [newEvent, setNewEvent] = React.useState(false);
+	const [updateEvent, setUpdateEvent] = React.useState(false);
+	const [CancelEvent, setCancelEvent] = React.useState(false);
+
+	const nameInput = (
+		<InputSection
+			title="Who is booking?"
+			description="i.e. takes all responsibility!"
+		>
+			<NameInput
 				peopleAllInfo={peopleAllInfo}
-				setUserSelected={setUserSelected}
 				userSelected={userSelected}
+				setUserSelected={setUserSelected}
+				setUserCount={setUserCount}
+				setDisabledRoomTypes={setDisabledRoomTypes}
 				setGearList={setGearList}
 			/>
-		</Paper>
+		</InputSection>
+	);
+
+	const eventDetailsInput = (
+		<InputSection title="Event Details">
+			<EventDetailsInput
+				facultyList={facultyList}
+				setSessionTitle={setSessionTitle}
+				setEventTypeSelected={setEventTypeSelected}
+				setFacultySelected={setFacultySelected}
+				setUsageSelected={setUsageSelected}
+			/>
+		</InputSection>
 	);
 
 	const timeInput = (
-		<Paper sx={{ maxWidth: 700, width: "90%", my: 2, mx: "auto", p: 2 }}>
-			<Box sx={{ textAlign: "left", m: 2, fontSize: 22, lineHeight: 2 }}>
-				Session Time
-				<FormLabel component="legend">
-					Based on your chosen Session Time, we wil notify you with the
-					availability of the room(s) selected above.
-				</FormLabel>
-			</Box>
-			<IndividualTimeInput
+		<InputSection
+			title="Session Time"
+			description="📌 Based on your chosen Session Time, we wil notify you with the availability of the room(s) selected above."
+		>
+			<TimeInput
 				setStartTimeSelected={setStartTimeSelected}
 				setEndTimeSelected={setEndTimeSelected}
 				setTimeCorrect={setTimeCorrect}
+				gearList={gearList}
+				setFilteredGearList={setFilteredGearList}
 			/>
-			<br />
-		</Paper>
+		</InputSection>
 	);
 
 	const gearInput = (
-		<Paper sx={{ maxWidth: 700, width: "90%", my: 2, mx: "auto", p: 2 }}>
-			<Box sx={{ textAlign: "left", m: 2, fontSize: 22, lineHeight: 2 }}>
-				Select Gear
-				<br />
-			</Box>
-			<IndividualGearCheckOut
+		<InputSection title="Gear Checkout">
+			<GearCheckOut
 				setGearSelected={setGearSelected}
-				gearList={gearList}
-				gear={gear}
-				setGear={setGear}
+				gearList={filteredGearList}
+				addGear={addGear}
+				setAddGear={setAddGear}
 				startTimeSelected={startTimeSelected}
 				endTimeSelected={endTimeSelected}
+			/>
+		</InputSection>
+	);
+
+	const formActions = (
+		<Box className="m-auto">
+			<FormActions
+				setNewEvent={setNewEvent}
+				setUpdateEvent={setUpdateEvent}
+				setCancelEvent={setCancelEvent}
+				setEventID={setEventID}
+				setIDError={setIDError}
+				setGoodID={setGoodID}
+				setUserSelected={setUserSelected}
 			/>
 			<br />
-		</Paper>
+		</Box>
 	);
-	return (
-		<div>
-			<Collapse in={true}>{nameInput}</Collapse>
-			<Collapse in={true}>{timeInput}</Collapse>
-			<Collapse in={true}>{gearInput}</Collapse>
 
-			<IndividualSubmit
-				userSelected={userSelected}
-				startTimeSelected={startTimeSelected}
-				endTimeSelected={endTimeSelected}
-				gearSelected={gearSelected}
-				timeCorrect={timeCorrect}
+	const requestEventID = (
+		<InputSection
+			title="Event Record ID"
+			description="Please enter the Event Record ID you received in the confirmation email before proceeding to the rest of the form."
+		>
+			<EventID
+				IDerror={IDerror}
+				setIDError={setIDError}
+				eventID={eventID}
+				setEventID={setEventID}
+				goodID={goodID}
+				setGoodID={setGoodID}
+				updateEvent={updateEvent}
+				CancelEvent={CancelEvent}
 			/>
-		</div>
+		</InputSection>
 	);
-}
 
-export default function Gear() {
 	return (
-		<div>
-			<Gear1 />
+		<div className="text-center">
+			<div className="mx-auto max-w-2xl">
+				<HeaderWithSubtitle
+					title="Select Gear"
+					subtitle="Add users to see a list of gear available to your access level in the
+					Select Gear menu."
+				/>
+				<SMCHours />
+				{formActions}
+				<Collapse in={newEvent || (updateEvent && goodID)}>
+					{nameInput}
+					{userCount > 0 && (
+						<>
+							<IframeSlide src="https://airtable.com/embed/shr7XfOauvLgRzajc" />
+							{eventDetailsInput}
+							{timeInput}
+
+							{timeCorrect && gearInput}
+						</>
+					)}
+				</Collapse>
+				{(updateEvent || CancelEvent) && (
+					<Collapse in>{requestEventID}</Collapse>
+				)}
+				{userCount > 0 && (newEvent || (updateEvent && goodID)) && (
+					<Submit
+						userSelected={userSelected}
+						setUserSelected={setUserSelected}
+						sessionTitle={sessionTitle}
+						setSessionTitle={setSessionTitle}
+						eventTypeSelected={eventTypeSelected}
+						setEventTypeSelected={setEventTypeSelected}
+						facultySelected={facultySelected}
+						setFacultySelected={setFacultySelected}
+						usageSelected={usageSelected}
+						setUsageSelected={setUsageSelected}
+						startTimeSelected={startTimeSelected}
+						setStartTimeSelected={setStartTimeSelected}
+						endTimeSelected={endTimeSelected}
+						setEndTimeSelected={setEndTimeSelected}
+						courseSelected={courseSelected}
+						setCourseSelected={setCourseSelected}
+						gearSelected={gearSelected}
+						setGearSelected={setGearSelected}
+						eventID={eventID}
+						setEventID={setEventID}
+						newEvent={newEvent}
+						setNewEvent={setNewEvent}
+						updateEvent={updateEvent}
+						setUpdateEvent={setUpdateEvent}
+						CancelEvent={CancelEvent}
+						setCancelEvent={setCancelEvent}
+						setAddCourse={setAddCourse}
+						setAddGear={setAddGear}
+						setUserCount={setUserCount}
+						timeCorrect={timeCorrect}
+					/>
+				)}
+				<br />
+			</div>
 		</div>
 	);
 }
